54dc337ed0fc376b7ea45944267d016f92045222
Bayastan <bayastan@mancho.dev>
2023-06-09 11:58:42 +0600

    first commit

---
+++ lesson1/index.js
@@ -0,0 +1,11 @@
+// for
+// console.log(5 % 2);
+
+let div = document.querySelector(".table");
+
+for (let i = 1; i < 5; i++) {
+  for (let j = 1; j < 10; j++) {
+    div.innerHTML += `${i} * ${j} = ${i * j} <br>`;
+  }
+  div.innerHTML += "<hr> ";
+}
